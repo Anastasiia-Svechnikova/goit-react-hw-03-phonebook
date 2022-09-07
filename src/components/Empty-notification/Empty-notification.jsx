8738f96b9e35c1f62610e51dd3ad47f8db0787d7
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-09-07 19:31:39 +0300

    task 1 complted

---
+++ src/components/Empty-notification/Empty-notification.jsx
@@ -0,0 +1,5 @@
+export default function EmptyNotification() {
+    return(
+        <p>Sorry, there are no contacts here. You can add a new one above.</p>
+    )
+}
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,4 @@
+import "./section.css"
+export default function Section({ children }) {
+	return <section className="section">{children}</section>;
+}
